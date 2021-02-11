ccde4ffede0b86abb0793a021f4c8465d2fec525
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-02-11 21:43:18 +0530

    Update 11 Day5 SumuptoNRec.py

--- 11 Day5 SumuptoNRec.py
+++ 11 Day5 SumuptoNRec.py
@@ -1,7 +1,13 @@
 
 #  sum of n natural numbers 
-def sumupton(l):
+def sumupton(n):
 	if n==1:
 		return 1 
 	else:
-		return    
+		return  sumupton(n-1)+n
+# sum(9)+10
+# sum(8)+9+10
+# sumu(7)+8+9+10
+
+
+print(sumupton(10))
