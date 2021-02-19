7a92f53dd9394277813f6555fffe895f5ade1541
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-02-19 20:57:35 +0530

    Add files via upload

---
+++ 19 Day8 CheckPalindRec.py
@@ -0,0 +1,32 @@
+# Recursive function to check if `str[low…high]` is a palindrome or not
+def isPalindrome(str, low, high):
+ 
+    # base case
+    if low >= high:
+        return True
+ 
+    # return false if mismatch happens
+    if str[low] != str[high]:
+        return False
+ 
+    # move to the next pair
+    else:
+        return isPalindrome(str, low + 1, high - 1)
+    # isPalindrome("malayalam",0,8)  0!>=8
+    # isPalindrome("alayala",)    1!>=7
+    # layal 2 6 
+    # ayal 3 5 
+    # aya 4>=4  return True 
+    
+ 
+if __name__ == '__main__':
+ 
+    str = "malayalam"
+ 
+    if isPalindrome(str, 0, len(str) - 1):
+        print("Palindrome")
+    else:
+        print("Not Palindrome")
+ 
+
+
